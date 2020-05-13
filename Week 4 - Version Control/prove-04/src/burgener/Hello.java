a8ed36a67dce4fefb34d41b5f0a5dfdb57397fc7
Wesley <wesleyburgener@gmail.com>
2020-05-13 17:06:17 -0600

    Pushing sayHello function to GitHub

---
+++ Week 4 - Version Control/prove-04/src/burgener/Hello.java
@@ -0,0 +1,13 @@
+package burgener;
+
+public class Hello {
+
+    public void sayHello() {
+        System.out.println("Hello my name is Wes!");
+    }
+
+    public static void main(String[] args) {
+        Hello hello = new Hello();
+        hello.sayHello();
+    }
+}
