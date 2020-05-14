6d587817939c06ed634bb23e8db26a06e9bcc396
Emma A <emmaga17@byui.edu>
2020-05-14 16:07:16 -0600

    Emma says hello

--- Week 4 - Version Control/prove-04/src/burgener/Hello.java
+++ Week 4 - Version Control/prove-04/src/burgener/Hello.java
@@ -6,10 +6,16 @@ public class Hello {
         System.out.println("Hello my name is Wes!");
     }
 
+    public void sayHello2() {
+        System.out.println("Hello, Wes, my name is Emma.");
+    }
+
     public static void main(String[] args) {
         Hello hello = new Hello();
         Goodbye goodbye = new Goodbye();
         hello.sayHello();
         goodbye.sayGoodbye();
+        Hello2 hello2 = new Hello2();
+        hello2.sayHello2();
     }
 }
