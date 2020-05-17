4661b76c78eff924f4af1351a54d9684b8d781bc
jose-aquinof <65300636+jose-aquinof@users.noreply.github.com>
2020-05-17 00:51:09 -0600

    Update Hello.java

--- Week 4 - Version Control/prove-04/src/burgener/Hello.java
+++ Week 4 - Version Control/prove-04/src/burgener/Hello.java
@@ -5,6 +5,7 @@ public class Hello {
     public void sayHello() {
         System.out.println("Hello my name is Wes!");
         System.out.println("Hello, Wes, my name is Emma.");
+        System.out.println("Hello, my name is Jose")
     }
 
     public static void main(String[] args) {
